fc2963420334c87b6dad29729fbe3fa44b13ece9
Selma Copra <selma.copra@codescene.com>
2025-02-10 13:24:36 +0100

    add src/main/java/org/example/BadCodeExample.java

--- src/main/java/org/example/TrackedPositiveFile.java
+++ src/main/java/org/example/TrackedPositiveFile.java
@@ -2,18 +2,49 @@ package org.example;
 
 public class TrackedPositiveFile {
     public String checkEligibility(int age, boolean hasLicense, boolean hasInsurance, boolean isHealthy, int drivingExperience) {
-        if (age >= 18 && hasLicense && hasInsurance) {
-            if (isHealthy) {
-                if (drivingExperience >= 2) {
-                    return "Eligible for driving.";
-                } else {
-                    return "Ineligible due to insufficient driving experience.";
-                }
-            } else {
-                return "Ineligible due to health issues.";
-            }
-        } else {
-            return "Ineligible due to age, license, or insurance issues.";
+//        if (age >= 18 && hasLicense && hasInsurance) {
+//            if (isHealthy) {
+//                if (drivingExperience >= 2) {
+//                    return "Eligible for driving.";
+//                } else {
+//                    return "Ineligible due to insufficient driving experience.";
+//                }
+//            } else {
+//                return "Ineligible due to health issues.";
+//            }
+//        }
+//
+//
+//        if (age >= 18 && hasLicense && hasInsurance) {
+//            if (isHealthy) {
+//                if (drivingExperience >= 2) {
+//                    return "Eligible for driving.";
+//                } else {
+//                    return "Ineligible due to insufficient driving experience.";
+//                }
+//            } else {
+//                return "Ineligible due to health issues.";
+//            }
+//        }
+//
+//
+//        if (age >= 18 && hasLicense && hasInsurance) {
+//            if (isHealthy) {
+//                if (drivingExperience >= 2) {
+//                    return "Eligible for driving.";
+//                } else {
+//                    return "Ineligible due to insufficient driving experience.";
+//                }
+//            }
+//        }
+
+        return "Ineligible due to health issues.";
+
+    }
+
+    private void testing(int a, int b, int c, int d, int e){
+        if(a>5 && b <6 && c<4){
+
         }
     }
 }
