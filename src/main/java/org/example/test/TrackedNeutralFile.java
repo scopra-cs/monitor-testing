55a8ad7eadec8680f8f649e0dfecca561177cc19
Selma Copra <selma.copra@codescene.com>
2025-02-10 14:18:11 +0100

    added duplicate name file

---
+++ src/main/java/org/example/test/TrackedNeutralFile.java
@@ -0,0 +1,8 @@
+package org.example.test;
+
+//File used to test having files with the same name inside same project
+public class TrackedNeutralFile {
+    void testing(String a, String b, String c, String d, String e){
+
+    }
+}
