edc154c2473f449e51435806ab7d4c684f218a7e
Selma Copra <selma.copra@codescene.com>
2025-02-10 14:19:46 +0100

    add example file with N/A score

---
+++ src/main/java/org/example/AppConfig.kt
@@ -0,0 +1,7 @@
+package org.example
+
+object AppConfig {
+    const val APP_NAME = "CodeScene IntelliJ Plugin"
+    const val VERSION = "1.0.0"
+    const val SUPPORT_EMAIL = "support@codescene.com"
+}
